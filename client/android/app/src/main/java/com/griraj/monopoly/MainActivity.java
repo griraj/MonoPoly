eaf446908df3627876cd57cd5eb730c090b20d80
Gri raj <griraj1238@gmail.com>
2026-08-06 22:25:40 +0500

    Add Android/Capacitor build

---
+++ client/android/app/src/main/java/com/griraj/monopoly/MainActivity.java
@@ -0,0 +1,5 @@
+package com.griraj.monopoly;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.griraj.monopoly',
+  appName: 'MonoPoly',
+  webDir: 'dist'
+};
+
+export default config;
